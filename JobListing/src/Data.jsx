5f79c04e278cb0258c2d4b43b84ffa7a897593fa
AwaisYounas72 <151017136+AwaisYounas72@users.noreply.github.com>
2023-12-19 09:10:37 +0500

    Add files via upload

---
+++ JobListing/src/Data.jsx
@@ -0,0 +1,115 @@
+const Data = [
+    {
+        id: 1,
+        imgURL: "/images/photosnap.svg",
+        title: "Senior Frontend Developer",
+        company: "Photosnap",
+        tags: ["Frontend", "Senior", "HTML", "CSS", "Javascript"],
+        jobType: "Full-Time",
+        postTime: "1d ago",
+        location: "USA only",
+        Featured: true
+    },
+    {
+        id: 2,
+        imgURL: "/images/manage.svg",
+        title: "Fullstack Developer",
+        company: "Manage",
+        tags: ["Fullstack", "Midweight", "HTML", "Python", "React"],
+        jobType: "Part-Time",
+        postTime: "1d ago",
+        location: "Remote",
+        Featured: true
+    },
+    {
+        id: 3,
+        imgURL: "/images/account.svg",
+        title: "Junior Frontend Developer",
+        company: "Account",
+        tags: ["Frontend", "Junior", "Javascript", "Sass", "React"],
+        jobType: "Part-Time",
+        postTime: "2d ago",
+        location: "USA only",
+        Featured: false
+    },
+    {
+        id: 4,
+        imgURL: "/images/myhome.svg",
+        title: "Junior Frontend Developer",
+        company: "My Home",
+        tags: ["Frontend", "Junior", "Javascript", "CSS"],
+        jobType: "Contract",
+        postTime: "5d ago",
+        location: "USA only",
+        Featured: false
+    },
+    {
+        id: 5,
+        imgURL: "/images/loop-studios.svg",
+        title: "Software Engineer",
+        company: "Loop Studios",
+        tags: ["Fullstack", "Midweight", "Javascript", "Sass", "Ruby"],
+        jobType: "Full-Time",
+        postTime: "1w ago",
+        location: "UK only",
+        Featured: false
+    },
+    {
+        id: 6,
+        imgURL: "/images/faceit.svg",
+        title: "Junior Backend Developer",
+        company: "Loop Studios",
+        tags: ["Backend", "Junior", "RoR", "Ruby"],
+        jobType: "Full-Time",
+        postTime: "2w ago",
+        location: "UK only",
+        Featured: false
+    },
+    {
+        id: 7,
+        imgURL: "/images/shortly.svg",
+        title: "Junior Developer",
+        company: "Shortly",
+        tags: ["Frontend", "Junior", "HTML", "Sass", "Javascript"],
+        jobType: "Full-Time",
+        postTime: "2w ago",
+        location: "Worldwide",
+        Featured: false
+    },
+    {
+        id: 8,
+        imgURL: "/images/insure.svg",
+        title: "Junior Frontend Developer",
+        company: "Insure",
+        tags: ["Frontend", "Junior", "Vue", "Sass", "Javascript"],
+        jobType: "Full-Time",
+        postTime: "2w ago",
+        location: "USA only",
+        Featured: false
+    },
+    {
+        id: 9,
+        imgURL: "/images/eyecam-co.svg",
+        title: "Full Stack Developer",
+        company: "Eyecam Co.",
+        tags: ["Fullstack", "Midweight", "Django", "Python", "Javascript"],
+        jobType: "Full-Time",
+        postTime: "3w ago",
+        location: "Worldwide",
+        Featured: false
+    },
+    {
+        id: 10,
+        imgURL: "/images/the-air-filter-company.svg",
+        title: "Front-end Dev",
+        company: "The Air Filter Company",
+        tags: ["Frontend", "Junior", "React", "Sass", "Javascript"],
+        jobType: "Part-Time",
+        postTime: "1mo ago",
+        location: "Worldwide",
+        Featured: false
+    },
+
+]
+
+export default Data
